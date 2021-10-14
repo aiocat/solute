511b062af5deab03cd227f309f5829ec3b1e9ba1
aiocat <aiocat@protonmail.com>
2021-10-14 19:11:47 +0300

    added avatar struct.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#[derive(Debug)]
+pub struct Avatar {
+    pub nickname: String
+}
+
+impl Avatar {
+    pub fn new(nickname: &str) -> Avatar {
+        Avatar {
+            nickname: nickname.to_owned()
+        }
     }
 }
